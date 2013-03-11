1ff09191feca1f8c54511e96a2ad248782e88a2f
Asko Soukka <asko.soukka@iki.fi>
2013-03-11 19:32:02 +0200

    Add sauce setup

---
+++ src/example/product/testing_robot.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import base64
+import httplib
+import os
+
+try:
+    import json
+    json  # pyflakes
+except ImportError:
+    import simplejson as json
+
+
+class Keywords(object):
+
+    def report_sauce_status(self, job_id, test_status, test_tags=[]):
+
+        username = os.environ.get('SAUCE_USERNAME')
+        access_key = os.environ.get('SAUCE_ACCESS_KEY')
+
+        if not job_id:
+            return u"No Sauce job id found. Skipping..."
+        elif not username or not access_key:
+            return u"No Sauce environment variables found. Skipping..."
+
+        token = base64.encodestring('%s:%s' % (username, access_key))[:-1]
+        body = json.dumps({'passed': test_status == 'PASS',
+                           'tags': test_tags})
+
+        connection = httplib.HTTPConnection('saucelabs.com')
+        connection.request('PUT', '/rest/v1/%s/jobs/%s' % (
+            username, job_id), body,
+            headers={'Authorization': 'Basic %s' % token}
+        )
+
+        return connection.getresponse().status
