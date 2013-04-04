714ef964a86d655a00a92c4e992fd91a954d2ea4
Asko Soukka <asko.soukka@iki.fi>
2013-04-05 00:28:13 +0300

    Fix tests

--- src/example/product/tests/test_robot.py
+++ src/example/product/tests/test_robot.py
@@ -8,8 +8,6 @@ def test_suite():
     suite = unittest.TestSuite()
     suite.addTests([
         layered(robotsuite.RobotTestSuite("test_product.robot"),
-                layer=EXAMPLE_PRODUCT_FUNCTIONAL_TESTING),
-        layered(robotsuite.RobotTestSuite("test_hello.robot"),
                 layer=EXAMPLE_PRODUCT_FUNCTIONAL_TESTING)
     ])
     return suite
